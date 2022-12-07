c8691968eaefab2b4a46f3ad44fd296b6a486812
ShahzaibMumtaz07 <shahzaib.mumtaz.20195@gmail.com>
2022-12-08 00:27:39 +0500

    updated views

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import InputForm
+from django.utils.translation import get_language
 
-# Create your views here.
+
+def index(request):
+    current_language = get_language()
+    form = InputForm(request.POST or None)
+    resp_data = {}
+    if form.is_valid():
+        resp_data = form.search_the_city(lang=current_language)
+    return render(request, "index.html", {"form": form, **resp_data})
